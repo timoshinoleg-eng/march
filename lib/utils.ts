b30611314426a4e28f4d87a52425135d93b33393
timoshinoleg-eng <timoshin.oleg@gmail.com>
2026-03-10 20:29:46 +0300

    Update lib/utils.ts from chatbot24-widget

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,28 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function generateId(): string {
-  return Math.random().toString(36).substring(2, 15)
-}
-
-export function formatDate(date: Date): string {
-  return new Intl.DateTimeFormat('ru-RU', {
-    hour: '2-digit',
-    minute: '2-digit'
-  }).format(date)
-}
-
-export function debounce<T extends (...args: unknown[]) => unknown>(
-  fn: T,
-  delay: number
-): (...args: Parameters<T>) => void {
-  let timeoutId: ReturnType<typeof setTimeout>
-  return (...args: Parameters<T>) => {
-    clearTimeout(timeoutId)
-    timeoutId = setTimeout(() => fn(...args), delay)
-  }
 }
